58be45e319a0f5d793bb25304f446d186b394102
Jeremiah Tabb <55849254+jtabb1@users.noreply.github.com>
2021-12-27 07:04:23 +0000

    Get a solution to work

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    int[] startAry = new int[] {0, 4, 7, 6, 2, 1, 3};
+    int tgt = 10;
+    int[] soln = new int[2];
+    Solution mySoln = new Solution();
+    soln = mySoln.twoSum(startAry, tgt);
+    System.out.println(
+      "["+soln[0]+", "+soln[1]+"]");
   }
 }
